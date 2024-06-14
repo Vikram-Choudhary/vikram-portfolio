b3c92b6e4895eda0789e0b3b179974d1d54728e0
Vikram Choudhary <vikramus4@gmail.com>
2024-06-14 02:14:12 -0400

    Added Mobile Menu and Home section
    Update html and css files and added js for menu show/hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,42 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
-
+if (navToggle) {
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.add("show-menu");
+  });
+}
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
-
+if (navClose) {
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show-menu");
+  });
+}
 /*==================== REMOVE MENU MOBILE ====================*/
-
-
+const navlink = document.querySelectorAll(".nav_link");
+function linkAction() {
+  document.getElementById("nav-menu").classList.remove("show-menu");
+}
+navlink.forEach((ele) => ele.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
-
 
 /*==================== QUALIFICATION TABS ====================*/
 
-
 /*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
