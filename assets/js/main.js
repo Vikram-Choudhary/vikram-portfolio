ab1bda06154d71b5a5c0e35695bffb986b2e834b
Vikram Choudhary <vikramus4@gmail.com>
2024-06-21 19:55:53 -0400

    swiper css updated and added next/prev button

--- assets/js/main.js
+++ assets/js/main.js
@@ -92,14 +92,15 @@ modelCloses.forEach((modalBtn, index) => {
 /*==================== PORTFOLIO SWIPER  ====================*/
 const swiper = new Swiper(".portfolio_container", {
   cssMode: true,
+  loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-  mousewheel: true,
   keyboard: true,
 });
 /*==================== TESTIMONIAL ====================*/
